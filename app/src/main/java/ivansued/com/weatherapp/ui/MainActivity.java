6ccdae049034470203894f032f0b5aeed6aeda1e
Ivan Sued <thefenry@gmail.com>
2015-04-09 09:04:54 -0700

    Added support for recyclerview and added Activity
    Added support for the recycler view for older versions of the os and the
    new activity for the hourly temperature is up.

--- app/src/main/java/ivansued/com/weatherapp/ui/MainActivity.java
+++ app/src/main/java/ivansued/com/weatherapp/ui/MainActivity.java
@@ -43,6 +43,7 @@ import ivansued.com.weatherapp.weather.Hour;
 public class MainActivity extends ActionBarActivity implements LocationListener {
     public static final String TAG = MainActivity.class.getSimpleName();
     public static final String DAILY_FORECAST = "DAILY_FORECAST";
+    private static final String HOURLY_FORECAST = "HOURLY_FORECAST";
     private Forecast mForecast;
 
     @InjectView(R.id.timeLabel) TextView mTimeLabel;
@@ -312,6 +313,13 @@ public class MainActivity extends ActionBarActivity implements LocationListener 
     public void startDailyActivity(View view){
         Intent intent = new Intent(this, DailyForecastActivity.class);
         intent.putExtra(DAILY_FORECAST, mForecast.getDailyForecast());
+        startActivity(intent);
+    }
+
+    @OnClick(R.id.hourlyButton)
+    public void startHourlyActivity(View view){
+        Intent intent = new Intent(this, HourlyForecastActivity.class);
+        intent.putExtra(HOURLY_FORECAST, mForecast.getHourlyForecast());
         startActivity(intent);
     }
 }
